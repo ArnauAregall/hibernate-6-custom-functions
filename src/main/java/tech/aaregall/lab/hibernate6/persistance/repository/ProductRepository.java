1d1744dc3a3e56a512aed67cbc23ad1e7bb0f0dd
Arnau Aregall <arnau.are@gmail.com>
2023-10-11 17:07:59 +0200

    Adds sample using custom function with JPA Specification

--- src/main/java/tech/aaregall/lab/hibernate6/persistance/repository/ProductRepository.java
+++ src/main/java/tech/aaregall/lab/hibernate6/persistance/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package tech.aaregall.lab.hibernate6.persistance.repository;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +11,7 @@ import java.util.Collection;
 import java.util.UUID;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, UUID> {
+public interface ProductRepository extends CrudRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
 
     /*
      * org.hibernate.query.SemanticException: Operand of 'member of' operator must be a plural path
@@ -23,6 +24,15 @@ public interface ProductRepository extends CrudRepository<Product, UUID> {
      */
     @Query("select p from Product p where array_contains(p.tags, :tag)")
     Collection<Product> findAllByTagsContaining(@Param("tag") String tag);
+
+    default Collection<Product> findAllByTagsContaining_Specification(final String tag) {
+        return findAll((root, criteriaQuery, criteriaBuilder) ->
+                criteriaBuilder.isTrue(
+                        criteriaBuilder.function("array_contains", Boolean.class,
+                                root.get("tags").as(String[].class), criteriaBuilder.literal(tag))
+                )
+        );
+    }
 
 
 }
--- src/test/java/tech/aaregall/lab/hibernate6/persistance/repository/ProductRepositoryTest.java
+++ src/test/java/tech/aaregall/lab/hibernate6/persistance/repository/ProductRepositoryTest.java
@@ -1,10 +1,12 @@
 package tech.aaregall.lab.hibernate6.persistance.repository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import tech.aaregall.lab.hibernate6.persistance.entity.Product;
 
+import java.util.Collection;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,30 +17,57 @@ class ProductRepositoryTest {
 	@Autowired
 	private ProductRepository productRepository;
 
+	@BeforeEach
+	void beforeEach() {
+		productRepository.deleteAll();
+	}
+
 	@Test
 	void findAllByTagsContaining() {
+		productRepository.saveAll(List.of(
+				Product.builder()
+						.sku("sku-111")
+						.name("Product 111 Test")
+						.price(105.99f)
+						.tags(new String[] {"foo", "bar"})
+						.build(),
+				Product.builder()
+						.sku("sku-222")
+						.name("Product 222 Test")
+						.price(10.0f)
+						.tags(new String[] {"foo", "baz"})
+						.build()));
 
-		Product product1 = Product.builder()
-				.sku("sku-1234")
-				.name("1234 Test")
-				.price(105.99f)
-				.tags(new String[] {"foo", "bar"})
-				.build();
+		final Collection<Product> result = productRepository.findAllByTagsContaining("baz");
 
-		Product product2 = Product.builder()
-				.sku("sku-4567")
-				.name("4567 Test")
-				.price(10.0f)
-				.tags(new String[] {"foo", "baz"})
-				.build();
-
-		productRepository.saveAll(List.of(product1, product2));
-
-		assertThat(productRepository.findAllByTagsContaining("baz"))
-				.asList()
+		assertThat(result)
 				.hasSize(1)
-				.extracting("sku")
-				.containsExactly("sku-4567");
+				.extracting(Product::getSku)
+				.containsExactly("sku-222");
+	}
+
+	@Test
+	void findAllByTagsContaining_Specification() {
+		productRepository.saveAll(List.of(
+				Product.builder()
+						.sku("sku-333")
+						.name("Product 333 Test")
+						.price(99f)
+						.tags(new String[] {"foo", "bar"})
+						.build(),
+				Product.builder()
+						.sku("sku-444")
+						.name("Product 444 Test")
+						.price(1000f)
+						.tags(new String[] {"foo", "baz"})
+						.build()));
+
+		final Collection<Product> result = productRepository.findAllByTagsContaining_Specification("baz");
+
+		assertThat(result)
+				.hasSize(1)
+				.extracting(Product::getSku)
+				.containsExactly("sku-444");
 	}
 
 }
